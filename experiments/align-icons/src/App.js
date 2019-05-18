5dc8742f9e80e8eef9885e0ee507d9db1ee30dd2
Guillaume Claret <dev@clarus.me>
2019-05-18 11:05:25 +0200

    Add a primitive mechanism of drag & drop with the mouse

--- experiments/align-icons/src/App.js
+++ experiments/align-icons/src/App.js
@@ -5,33 +5,78 @@ import items from './items';
 
 export default class App extends PureComponent {
   state = {
-    id: items[0].id
+    id: items[0].id,
+    mouseCurrentX: null,
+    mouseStartX: null
   };
+
+  getItemIndex() {
+    const {id} = this.state;
+
+    return items.findIndex(item => item.id === id);
+  }
 
   handleClick = (id) => {
     this.setState({id});
   };
 
+  handleMouseDown = (event) => {
+    console.log('Down');
+    this.setState({mouseCurrentX: event.clientX, mouseStartX: event.clientX});
+  };
+
+  handleMouseMove = (event) => {
+    this.setState({mouseCurrentX: event.clientX});
+    const {mouseStartX} = this.state;
+
+    if (typeof mouseStartX === 'number') {
+      console.log('Move', event.clientX - this.state.mouseStartX);
+    }
+  };
+
+  handleMouseLeaveOrUp = () => {
+    console.log('Up');
+    const {mouseCurrentX, mouseStartX} = this.state;
+    const shiftPixels = mouseCurrentX - mouseStartX;
+    const shiftIndex = Math.round(shiftPixels / 120);
+    const newUnboundedIndex = this.getItemIndex() - shiftIndex;
+    const newIndex = Math.max(Math.min(newUnboundedIndex, items.length - 1), 0);
+    this.setState({
+      id: items[newIndex].id,
+      mouseCurrentX: null,
+      mouseStartX: null
+    });
+  };
+
   render() {
-    const {id} = this.state;
-    const itemIndex = items.findIndex(item => item.id === id);
-    const shift = (items.length - 1) / 2 - itemIndex;
+    const {mouseCurrentX, mouseStartX} = this.state;
+    const shiftIndexPixels = 120 * ((items.length - 1) / 2 - this.getItemIndex());
+    const shiftDragPixels = mouseCurrentX && mouseStartX ? mouseCurrentX - mouseStartX : 0;
 
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Drag & Drop</h1>
           <p>↓</p>
-          <div style={{
-            display: 'flex',
-            transform: `translate(${shift * 120}px, 0px)`,
-            transition: 'transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s',
-          }}>
+          <div
+            onMouseDown={this.handleMouseDown}
+            onMouseLeave={this.handleMouseLeaveOrUp}
+            onMouseMove={this.handleMouseMove}
+            onMouseUp={this.handleMouseLeaveOrUp}
+            style={{
+              cursor: 'pointer',
+              display: 'flex',
+              transform: `translate(${shiftIndexPixels + shiftDragPixels}px, 0px)`,
+              ...(!(mouseCurrentX && mouseStartX)
+                ? {transition: 'transform 0.35s cubic-bezier(0.15, 0.3, 0.25, 1) 0s'}
+                : {}
+              ),
+            }}
+          >
             {items.map(item => 
               <Item
-                id={item.id}
                 key={item.id}
                 message={item.message}
-                onClick={this.handleClick}
               />
             )}
           </div>
--- experiments/align-icons/src/Item.js
+++ experiments/align-icons/src/Item.js
@@ -1,23 +1,11 @@
 import React, {PureComponent} from 'react';
 
 export default class Item extends PureComponent {
-  handleClick = () => {
-    const {id, onClick} = this.props;
-
-    onClick(id);
-  };
-
   render() {
     const {message} = this.props;
 
     return (
-      <p
-        onClick={this.handleClick}
-        style={{
-          cursor: 'pointer',
-          width: 120
-        }}
-      >
+      <p style={{width: 120}}>
         {message}
       </p>
     );
